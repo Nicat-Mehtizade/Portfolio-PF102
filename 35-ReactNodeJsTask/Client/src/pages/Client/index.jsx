4b86067a0e5bdf61bd104b68ede24e6feb151c55
Nicat-Mehtizade <nicat.mehdizadeh@gmail.com>
2025-03-17 00:37:03 +0400

    Feat | Fe | SearchAndSortAdded

--- 35-ReactNodeJsTask/Client/src/pages/Client/index.jsx
+++ 35-ReactNodeJsTask/Client/src/pages/Client/index.jsx
@@ -3,9 +3,18 @@ import axios from "axios"
 
 const Client = () => {
   const [blogs, setBlogs] = useState([]);
+  const [search, setSearch] = useState("");
+  const [sortBy, setSortBy] = useState("");
+  const [order, setOrder] = useState("");
   const Blogs = async () => {
     try {
-      const response = await axios("http://localhost:8000/api/blogs");
+      let query = new URLSearchParams();
+
+      if (search) query.append("title", search);
+      if (sortBy) query.append("sortBy", sortBy);
+      if (order) query.append("order", order);
+
+      const response = await axios(`http://localhost:8000/api/blogs?${query.toString()}`);
       console.log(response.data.data);
       setBlogs(response.data.data);
     } catch (error) {
@@ -14,10 +23,34 @@ const Client = () => {
   };
   useEffect(() => {
     Blogs();
-  }, []);
+  }, [search, sortBy, order]);
+
   return (
     <div className='bg-[#F2F2F2] py-20'>
     <div className='max-w-[1400px] mx-auto'>
+    <div className="flex justify-between items-center mb-6 px-4">
+          <input
+            type="text"
+            placeholder="Search blogs..."
+            className="border p-2 w-1/3 rounded-md"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+
+          <div className="flex gap-4">
+            <select className="border p-2 rounded-md" onChange={(e) => setSortBy(e.target.value)}>
+              <option value="" selected disabled>Sort By</option>
+              <option value="title">Title</option>
+              <option value="date">Date</option>
+            </select>
+
+            <select className="border p-2 rounded-md" onChange={(e) => setOrder(e.target.value)}>
+              <option value="">Order</option>
+              <option value="asc">Ascending</option>
+              <option value="desc">Descending</option>
+            </select>
+          </div>
+        </div>
       <div className='flex flex-wrap gap-12 justify-center '>
         {blogs ? (
           blogs.map((blog)=>{
